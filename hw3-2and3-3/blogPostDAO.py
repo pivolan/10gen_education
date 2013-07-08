b156b36ed32f5d576f4aa40a30030596a5f2cfe5
pivolan <pivolan@gmail.com>
2013-07-08 14:01:11 +0400

    3lesson hw3.3

--- hw3-2and3-3/blogPostDAO.py
+++ hw3-2and3-3/blogPostDAO.py
@@ -21,6 +21,7 @@ __author__ = 'aje'
 import sys
 import re
 import datetime
+import pymongo
 
 
 
@@ -55,6 +56,7 @@ class BlogPostDAO:
         # now insert the post
         try:
             # XXX HW 3.2 Work Here to insert the post
+            self.posts.insert(post)
             print "Inserting the post"
         except:
             print "Error inserting post"
@@ -68,7 +70,7 @@ class BlogPostDAO:
         cursor = []         # Placeholder so blog compiles before you make your changes
 
         # XXX HW 3.2 Work here to get the posts
-
+        cursor = self.posts.find().sort([('date', pymongo.DESCENDING)]).limit(num_posts)
         l = []
 
         for post in cursor:
@@ -92,7 +94,7 @@ class BlogPostDAO:
 
         post = None
         # XXX Work here to retrieve the specified post
-
+        post = self.posts.find_one({'permalink': permalink})
         if post is not None:
             # fix up date
             post['date'] = post['date'].strftime("%A, %B %d %Y at %I:%M%p")
@@ -110,7 +112,7 @@ class BlogPostDAO:
         try:
             last_error = {'n':-1}           # this is here so the code runs before you fix the next line
             # XXX HW 3.3 Work here to add the comment to the designated post
-
+            self.posts.update({'permalink': permalink}, {'$push':{'comments': comment}})
 
             return last_error['n']          # return the number of documents updated
 
